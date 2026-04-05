b1f73c0914755597b018d180da3da773147d51ac
KevinM123456789 <kmonroy5178@gmail.com>
2026-04-05 19:23:29 -0400

    Update use-peer-voice.ts

--- hooks/use-peer-voice.ts
+++ hooks/use-peer-voice.ts
@@ -115,7 +115,27 @@ export function usePeerVoice(options: UsePeerVoiceOptions) {
 
       const peer = new Peer(peerId, {
         debug: 0,
-        config: { iceServers: [{ urls: "stun:stun.l.google.com:19302" }, { urls: "stun:stun1.l.google.com:19302" }] },
+        config: {
+        iceServers: [
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun1.l.google.com:19302" },
+          {
+            urls: "turn:openrelay.metered.ca:80",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+          {
+            urls: "turn:openrelay.metered.ca:443?transport=tcp",
+            username: "openrelayproject",
+            credential: "openrelayproject",
+          },
+        ],
+      },
       });
 
       peer.on("open", (id) => {
